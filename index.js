5e5a52f082642fadf7d10a79bd4261e163e78cf3
yekogarcia <yekogarcia@yahoo.com>
2024-07-18 18:20:33 -0500

    config cors

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const {logErrors, errorhandler} = require('./middlewares/error.handler');
-const routerApi= require('./router')
+const cors = require('cors');
+const { logErrors, errorhandler } = require('./middlewares/error.handler');
+const routerApi = require('./router')
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+}));
 app.use(logErrors);
 app.use(errorhandler);
 
 routerApi(app)
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log("mi puerto es " + port);
 })
