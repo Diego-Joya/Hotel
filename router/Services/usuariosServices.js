b257cc739fe599f07d1ab3945641079a7e8df503
Diego-Joya <joya.d.f.o2017@gmail.com>
2024-07-28 14:02:54 -0500

    ajuste clientes

--- router/Services/usuariosServices.js
+++ router/Services/usuariosServices.js
@@ -1,6 +1,9 @@
+const pool = require('../../libs/postgres.pool')
+
 class usuariosServices{
     constructor(){
-
+this.pool=pool;
+this.pool.on('error',(err)=>console.log(err));
     }
 }
 module.exports = usuariosServices;
