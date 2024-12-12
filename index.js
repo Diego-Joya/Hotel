760d0a528daea510eac334518b817d6ce6958d4f
Diego-Joya <joya.d.f.o2017@gmail.com>
2024-12-11 23:23:24 -0500

    local strategy

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const { logErrors, errorhandler } = require('./middlewares/error.handler');
 const routerApi = require('./router')
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(cookieParser());
+
 app.use(cors({
   origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true,
 }));
 app.use(logErrors);
 app.use(errorhandler);
