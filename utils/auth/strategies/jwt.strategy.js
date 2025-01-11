c2e49b89d87b6eacdc0e9884304409cd66b5493e
Diego-Joya <joya.d.f.o2017@gmail.com>
2025-01-10 20:57:15 -0500

    local strategy

--- utils/auth/strategies/jwt.strategy.js
+++ utils/auth/strategies/jwt.strategy.js
@@ -12,6 +12,7 @@ const cookieExtractor = (req) => {
     let token = null;
     if (req && req.cookies) {
       token = req.cookies['token']; 
+      console.log('mira pendejoooooooooooo');
     }
     return token;
   };
