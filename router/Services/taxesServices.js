a4e0b85d88663e022b780ce545c8a338a38b099f
Diego-Joya <joya.d.f.o2017@gmail.com>
2026-06-21 21:33:00 -0500

    avances

--- router/Services/taxesServices.js
+++ router/Services/taxesServices.js
@@ -7,6 +7,7 @@ class taxesServices {
     this.pool = pool;
     this.pool.on('error', (err) => console.error(err));
   }
+
   async createTax(body) {
     const tax_name = body.tax_name;
     const tax_type = body.tax_type;
@@ -37,6 +38,63 @@ class taxesServices {
     }
 
   }
+
+  async updateTax(id, body) {
+    const tax_name = body.tax_name;
+    const tax_type = body.tax_type;
+    const tax_value = body.tax_value;
+    const is_automatic = body.is_automatic;
+    const description = body.description;
+
+    const updated_at = moment().format('YYYY-MM-DD HH:mm:ss');
+    try {
+      const query = `UPDATE booking_data.taxes
+  SET tax_name=$1, tax_type=$2, tax_value=$3, is_automatic=$4, description=$5, updated_at=$6
+  WHERE tax_id = $7 RETURNING *`;
+      const rta = await this.pool.query(query, [
+        tax_name,
+        tax_type,
+        tax_value,
+        is_automatic,
+        description,
+        updated_at,
+        id
+      ]);
+      return rta.rows;
+
+    } catch (error) {
+      return messageHandler(error);
+    }
+
+  }
+
+  async getAll(params) {
+    let where = `where 1=1`;
+    if (typeof params.company_id != 'undefined') {
+      where += ` and company_id = ${params.company_id}`;
+    }
+    if (typeof params.is_active != 'undefined') {
+      where += ` and is_active = ${params.is_active}`;
+    }
+    if (typeof params.tax_type != 'undefined') {
+      where += ` and tax_type = '${params.tax_type}'`;
+    }
+    if (typeof params.tax_name != 'undefined') {
+      where += ` and tax_name = '${params.tax_name}'`;
+    }
+    if (typeof params.description != 'undefined') {
+      where += ` and description = '${params.description}'`;
+    }
+
+    try {
+      const query = `SELECT * FROM booking_data.taxes ${where}`;
+      const rta = await this.pool.query(query);
+      return rta.rows;
+    } catch (error) {
+      return messageHandler(error);
+    }
+  }
+
 
 }
 
--- router/taxes.js
+++ router/taxes.js
@@ -2,20 +2,29 @@ const express = require('express');
 const router = express.Router();
 const taxesServices = require('./Services/taxesServices');
 const taxes = new taxesServices();
-// router.get('', async (req, res, next) => {
-//   try {
-
-//   } catch (error) {
-
-//   }
-// });
+router.get('', async (req, res, next) => {
+  try {
+    const params = req.query;
+    const result = await taxes.getAll(params);
+    if (typeof result.ok !== 'undefined' && result.ok === false) {
+      res.send(result);
+    } else {
+      res.json({
+        ok: true,
+        data: result,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('', async (req, res, next) => {
   try {
     const body = req.body;
     console.log(body);
     const result = await taxes.createTax(body);
-     console.log("result", result);
+    console.log("result", result);
     if (typeof result.ok !== 'undefined' && result.ok === false) {
       res.send(result);
     } else {
@@ -25,6 +34,30 @@ router.post('', async (req, res, next) => {
         data: result,
       });
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    console.log(body);
+    const result = await taxes.updateTax(id, body);
+    console.log("result", result);
+    if (typeof result.ok !== 'undefined' && result.ok === false) {
+      res.send(result);
+    } else {
+      res.json({
+        ok: true,
+        message: 'Impuesto actualizado correctamente',
+        data: result,
+        id,
+      });
+    }
+
   } catch (error) {
     next(error);
   }
